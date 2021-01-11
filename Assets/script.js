d3dfc1ae82426fef3bca4cd0a0a0545c65a495b3
Bardini <derekbardini@gmail.com>
2021-01-10 21:45:30 -0600

    need event listeners and local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 
 const now = moment().format('MMMM Do YYYY, h:mm a');
-var hr24 = moment().format('H');
-let hr12 = moment().format('h'); 
+var hr24 = moment().format('H'); 
 
 $("#currentDay").html(now);
 
@@ -10,21 +9,22 @@ let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
 
 if (storedPlans !== null) {
     planTextArr = storedPlans;
-  } else {
+  } 
+  else {
     planTextArr = new Array(12);
-    planTextArr[7] = "Time to smoke a little weed!";
+    planTextArr[1] = "Smoke weed everyday!";
   };
 
   let plannerDiv = $("#dailyplanner");
   plannerDiv.empty();
 
-  for (let hour = 9; hour <= 20; hour++) {
+  for (let hour = 9; hour <= 21; hour++) {
     let index = hour - 9;
 
     let rowDiv = $('<div>');
     rowDiv.addClass('row');
     rowDiv.addClass('plannerRow');
-    rowDiv.attr('hour-index',hour);
+    rowDiv.attr('hour-index', index);
 
     let TimeDiv = $('<div>');
     TimeDiv.addClass('col-md-2');
@@ -32,27 +32,88 @@ if (storedPlans !== null) {
     const timeBoxSpn = $('<span>');
     timeBoxSpn.attr('class','timeBox');
 
-    let displayHour = 0;
+    let displayHour = 12;
     let ampm = "";
-    if (hour > 12) { 
+
+    if (hour === 12) {
+      ampm = "pm";
+    } else if (hour >= 13) { 
       displayHour = hour - 12;
       ampm = "pm";
-    } else {
+    }
+    else {
       displayHour = hour;
       ampm = "am";
     }
-    console.log(displayHour + ampm);
 
     timeBoxSpn.text(displayHour + ampm);
 
     rowDiv.append(TimeDiv);
     TimeDiv.append(timeBoxSpn);
-    plannerDiv.append(rowDiv);
+    
     // timer row/ column now built
+    let dailyPlanSpn = $('<input>');
 
+    dailyPlanSpn.attr('id',`input-${index}`);
+    dailyPlanSpn.attr('hour-index',index);
+    dailyPlanSpn.attr('type','text');
+    // dailyPlanSpn.attr('class','dailyPlan');
+    dailyPlanSpn.val(planTextArr[index]);
 
+    let plannerTextDiv = $('<div>');
+    plannerTextDiv.addClass('col-md-9');
 
+    rowDiv.append(plannerTextDiv);
+    plannerTextDiv.append(dailyPlanSpn);
 
+    let saveDiv = $('<div>');
+    saveDiv.addClass('col-md-1');
+
+    let saveBtn = $('<button>');
+    saveBtn.attr('id',`saveid-${index}`);
+    saveBtn.attr('save-id',index);
+    saveBtn.attr('class',"far fa-save saveIcon");
+    // saveBtn.on("click", saveFunc)
+
+    rowDiv.append(saveDiv);
+    saveDiv.append(saveBtn);
+    plannerDiv.append(rowDiv);
+    
+    changeRowColor(rowDiv, hour);
+    
+    
 }
 
+function changeRowColor (hourRow,hour) { 
+
+  if ( hour < hr24) {
+    
+    hourRow.css("background-color","tomato")
+  } else if ( hour > hr24) {
+    hourRow.css("background-color","lightgreen")
+  } else {
+    hourRow.css("background-color","blue")
+  }
+};
+
+//   function saveFunc(event) {
+
+//   event.preventDefault();  
+//   console.log(this);
+
+//   let index = 0;
+
+//   let inputId = '#input-'+ index;
+//   let value = $(inputId).val();
+
+//   planTextArr[index] = value;
+
+
+// console.log('value ', value); 
+//  console.log('index ', index); 
+//  console.log('click pta after '+ planTextArr); 
+//  console.log(localStorage);
+
+//   localStorage.setItem("storedPlans", JSON.stringify(planTextArr));
+// };  
 });
