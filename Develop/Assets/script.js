62f361d36ece63d75812479d75f9f3d53d4e4b8c
Bardini <derekbardini@gmail.com>
2021-01-12 19:42:15 -0600

    deleted console logs

--- Develop/Assets/script.js
+++ Develop/Assets/script.js
@@ -1,33 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-const now = moment().format('MMMM Do YYYY, h:mm a');
-var hr24 = moment().format('H');
+  const now = moment().format('MMMM Do YYYY, h:mm a');
+  var hr24 = moment().format('H');
 
-$("#currentDay").text(now)
+  $("#currentDay").text(now)
 
-let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
-let planTextArr;
-if (storedPlans !== null) {
+  let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+  let planTextArr;
+  if (storedPlans !== null) {
     planTextArr = storedPlans;
     console.log("storage present");
-  } 
+  }
   else {
     planTextArr = new Array(12);
     planTextArr[1] = "Time for class!";
-    console.log("storage empty");
   };
 
-console.log(planTextArr);
+  let containerDiv = $(".container")
+  containerDiv.empty();
 
-let containerDiv = $(".container")
-containerDiv.empty();
-
-for (let hour = 9; hour <= 20; hour++) {
+  for (let hour = 9; hour <= 20; hour++) {
     let index = hour - 9;
 
     let rowDiv = $("<div>");
-  rowDiv.addClass("row");
-  rowDiv.attr('hour-index', index);
+    rowDiv.addClass("row");
+    rowDiv.attr('hour-index', index);
 
     let colmd2Div = $('<div>');
     colmd2Div.addClass('col-md-2');
@@ -35,15 +32,15 @@ for (let hour = 9; hour <= 20; hour++) {
     colmd2Div.addClass("hour");
 
     const timeBoxSpn = $('<span>');
-    timeBoxSpn.attr('class','timeBox');
-    
+    timeBoxSpn.attr('class', 'timeBox');
+
 
     let displayHour = 12;
     let ampm = "";
 
     if (hour === 12) {
       ampm = "pm";
-    } else if (hour >= 13) { 
+    } else if (hour >= 13) {
       displayHour = hour - 12;
       ampm = "pm";
     }
@@ -51,7 +48,7 @@ for (let hour = 9; hour <= 20; hour++) {
       displayHour = hour;
       ampm = "am";
     }
-   
+
     timeBoxSpn.text(displayHour + ampm);
 
     rowDiv.append(colmd2Div);
@@ -61,9 +58,9 @@ for (let hour = 9; hour <= 20; hour++) {
     colmd9Div.addClass('col-md-9');
 
     let dailyPlanSpn = $('<input>');
-    dailyPlanSpn.attr('id',`input-${index}`);
-    dailyPlanSpn.attr('hour-index',index);
-    dailyPlanSpn.attr('type','text');
+    dailyPlanSpn.attr('id', `input-${index}`);
+    dailyPlanSpn.attr('hour-index', index);
+    dailyPlanSpn.attr('type', 'text');
     dailyPlanSpn.addClass("textarea")
 
     dailyPlanSpn.val(planTextArr[index]);
@@ -77,30 +74,24 @@ for (let hour = 9; hour <= 20; hour++) {
     colmd1Div.css("background-color", "#1b1b1b")
 
     let saveBtn = $('<button>');
-    saveBtn.attr('id',`saveid-${index}`);
-    saveBtn.attr('save-id',index);
-    saveBtn.attr('class',"fa fa-save saveBtn");
+    saveBtn.attr('id', `saveid-${index}`);
+    saveBtn.attr('save-id', index);
+    saveBtn.attr('class', "fa fa-save saveBtn");
     saveBtn.on("click", saveFunc);
 
     rowDiv.append(colmd1Div);
     colmd1Div.append(saveBtn);
     containerDiv.append(rowDiv);
 
-    
-
-
     changeRowColor(rowDiv, hour);
-    console.log("working");
-}
+  }
 
+  function changeRowColor(hourRow, hour) {
 
+    if (hour < hr24) {
 
-function changeRowColor (hourRow,hour) { 
-
-    if ( hour < hr24) {
-      
       hourRow.addClass("past")
-    } else if ( hour > hr24) {
+    } else if (hour > hr24) {
       hourRow.addClass("future")
     } else {
       hourRow.addClass("present")
@@ -108,27 +99,23 @@ function changeRowColor (hourRow,hour) {
   };
 
   function saveFunc(event) {
-    event.preventDefault();  
+    event.preventDefault();
     index = this.getAttribute("save-id")
     otherindex = this.getAttribute("hour-index")
 
-    
     var input = document.getElementById('input-' + index).value
-    planTextArr[index]= input;
+    planTextArr[index] = input;
     console.log(planTextArr);
-    localStorage.setItem("storedPlans",JSON.stringify(planTextArr));
+    localStorage.setItem("storedPlans", JSON.stringify(planTextArr));
     console.log(planTextArr);
 
-    //add text from input to planTextArray
-    
-  
-     }
+  }
 
-     let clearButton = $("#clear-button")
-     clearButton.on("click", function(){
-       console.log("working");
-       localStorage.clear();
-       location.reload();
-     })
+  let clearButton = $("#clear-button")
+  clearButton.on("click", function () {
+    console.log("working");
+    localStorage.clear();
+    location.reload();
+  })
 
 })
